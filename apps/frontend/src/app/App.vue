f5c7b7f974c359206994a93ae2cdb1eadccbdb82
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-02-03 14:20:50 +0100

    virtualize item grid

--- apps/frontend/src/app/App.vue
+++ apps/frontend/src/app/App.vue
@@ -2,8 +2,8 @@
   <main class="h-dvh grid grid-cols-[auto_520px] text-[#d2d2d2]">
     <ThePlanningCanvas />
 
-    <TheRightSidebar v-if="isFinished" class="overflow-auto">
-      <label class="flex flex-col">
+    <TheRightSidebar v-if="isFinished" class="flex flex-col overflow-auto">
+      <label class="mb-4 flex flex-col">
         <span class="mb-1 text-xl">Search</span>
         <input
           v-model="searchText"
@@ -12,21 +12,19 @@
         />
       </label>
 
-      <RightSidebarItem
-        v-for="item in shownWeapons"
-        :key="item.name"
-        :item="item"
-      />
+      <RightSidebarItemGrid
+        :items="shownWeapons"
+        class="h-full"
+      ></RightSidebarItemGrid>
     </TheRightSidebar>
   </main>
 </template>
 
 <script setup lang="ts">
 import { useFetch } from "@vueuse/core"
-
 import ThePlanningCanvas from "./components/ThePlanningCanvas.vue"
 import TheRightSidebar from "./components/TheRightSidebar.vue"
-import RightSidebarItem from "./components/RightSidebarItem.vue"
+import RightSidebarItemGrid from "./components/RightSidebarItemGrid.vue"
 import type { SavedWikiData } from "@baldurs-gate-3-walkthrough-planner/types"
 
 const searchText = ref("")
---
+++ apps/frontend/src/app/components/RightSidebarItemGrid.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-bind="containerProps">
+    <div v-bind="wrapperProps">
+      <div v-for="row in list" :key="row.index" class="mb-2 flex gap-2">
+        <RightSidebarItem
+          v-for="item in row.data"
+          :key="item.name"
+          :item="item"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Item } from "@baldurs-gate-3-walkthrough-planner/types"
+import { useVirtualList } from "@vueuse/core"
+import type { PropType } from "vue"
+import { chunk } from "lodash-es"
+import RightSidebarItem from "./RightSidebarItem.vue"
+
+const props = defineProps({
+  items: {
+    type: Array as PropType<Item[]>,
+  },
+})
+
+watch(toRef(props, "items"), () => scrollTo(0))
+
+const { list, containerProps, wrapperProps, scrollTo } = useVirtualList(
+  computed(() => chunk(props.items, 4)),
+  {
+    itemHeight: 118,
+    overscan: 20,
+  },
+)
+</script>
