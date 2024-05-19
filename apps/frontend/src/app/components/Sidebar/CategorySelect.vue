502ce5cb10e5c952580aed24eb2e0a58c53e2f89
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-05-19 14:48:17 +0200

    remove category search box

--- apps/frontend/src/app/components/Sidebar/CategorySelect.vue
+++ apps/frontend/src/app/components/Sidebar/CategorySelect.vue
@@ -1,10 +1,5 @@
 <template>
-  <Dropdown
-    option-label="name"
-    option-value="name"
-    scroll-height="400px"
-    filter
-  >
+  <Dropdown option-label="name" option-value="name" scroll-height="400px">
     <template #option="{ option }">
       <div class="flex items-center gap-1.5">
         <img :src="option.icon" width="44" height="44" />
