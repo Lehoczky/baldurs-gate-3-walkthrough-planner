9d63061080c689db9791060995ce46907276330b
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-12-28 17:55:25 +0100

    fix primevue deprecation warning

--- packages/planner/src/components/Sidebar/CategorySelect.vue
+++ packages/planner/src/components/Sidebar/CategorySelect.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import Dropdown from "primevue/dropdown"
+import Select from "primevue/select"
 </script>
 
 <template>
-  <Dropdown option-label="name" option-value="name" scroll-height="400px">
+  <Select option-label="name" option-value="name" scroll-height="400px">
     <template #option="{ option }">
       <div class="flex items-center gap-1.5">
         <img :src="option.icon" width="44" height="44" />
         <div class="capitalize">{{ option.name }}</div>
       </div>
     </template>
-  </Dropdown>
+  </Select>
 </template>
