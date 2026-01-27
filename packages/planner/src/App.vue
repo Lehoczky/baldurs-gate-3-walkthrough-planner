e1564ad7b26aeda236d8ead0ffb09bc12a46b3b6
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2026-01-27 22:27:30 +0100

    resizable sidebar

--- packages/planner/src/App.vue
+++ packages/planner/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { useEventListener } from "@vueuse/core"
 import { storeToRefs } from "pinia"
+import Splitter from "primevue/splitter"
+import SplitterPanel from "primevue/splitterpanel"
 import Toast from "primevue/toast"
+import { computed } from "vue"
 
 import FloatingDataFilters from "@/data-filters/FloatingDataFilters.vue"
 import TheFlow from "@/flowchart/TheFlow.vue"
@@ -30,22 +33,37 @@ function warnWhenLeavingTheSiteWithUnsavedChanges(event: BeforeUnloadEvent) {
 }
 
 const breakpoints = useBreakpoints()
-const showFloatingFilters = breakpoints.smallerOrEqual("sm")
+const isSmallScreen = breakpoints.smallerOrEqual("sm")
+const splitterLayout = computed(() => {
+  return isSmallScreen.value ? "vertical" : "horizontal"
+})
+const splitterStorageKey = import.meta.env.DEV ? null : "planner:splitter-size"
 
 fetchStoreData()
 </script>
 
 <template>
   <div>
-    <main
-      class="grid h-dvh text-[#d2d2d2] max-lg:grid-rows-[55%_auto] lg:grid-cols-[auto_528px]"
-    >
+    <main class="grid text-[#d2d2d2]">
       <FloatingDataFilters
-        v-if="showFloatingFilters"
+        v-if="isSmallScreen"
         class="absolute top-0 right-0 left-0 z-20"
       />
-      <TheFlow />
-      <TheSidebar v-if="storeLoaded" />
+
+      <Splitter
+        :layout="splitterLayout"
+        :state-key="splitterStorageKey"
+        state-storage="local"
+        class="h-dvh"
+      >
+        <SplitterPanel :size="75">
+          <TheFlow />
+        </SplitterPanel>
+
+        <SplitterPanel v-if="storeLoaded">
+          <TheSidebar class="h-dvh lg:min-w-[506px]" />
+        </SplitterPanel>
+      </Splitter>
     </main>
 
     <Toast position="bottom-left" />
