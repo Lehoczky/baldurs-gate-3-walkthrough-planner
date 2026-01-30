64574493ebf3a93df51880222acfffc0e22c1e3a
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2026-01-31 00:30:44 +0100

    companions and locations

--- packages/wiki-scraper/src/index.ts
+++ packages/wiki-scraper/src/index.ts
@@ -3,6 +3,8 @@ import { chromium } from "playwright"
 import { parseArgs } from "./args.ts"
 import { getAmulets } from "./pages/amulets.ts"
 import { getArmours } from "./pages/armours.ts"
+import { getCompanions } from "./pages/companions.ts"
+import { getLocations } from "./pages/locations.ts"
 import { getWeapons } from "./pages/weapons.ts"
 import { getSavedData, saveData } from "./saved-data.ts"
 
@@ -15,23 +17,23 @@ const page = await browser.newPage()
 if (shouldScrape("amulets")) savedData.amulets = await getAmulets(page)
 if (shouldScrape("armours")) savedData.armours = await getArmours(page)
 // TODO
-if (shouldScrape("arrows")) savedData.armours = await getArmours(page)
-if (shouldScrape("bosses")) savedData.armours = await getArmours(page)
-if (shouldScrape("cloaks")) savedData.armours = await getArmours(page)
-if (shouldScrape("clothes")) savedData.armours = await getArmours(page)
-if (shouldScrape("coatings")) savedData.armours = await getArmours(page)
-if (shouldScrape("companions")) savedData.armours = await getArmours(page)
-if (shouldScrape("elixirs")) savedData.armours = await getArmours(page)
-if (shouldScrape("footwears")) savedData.armours = await getArmours(page)
-if (shouldScrape("grenades")) savedData.armours = await getArmours(page)
-if (shouldScrape("handwears")) savedData.armours = await getArmours(page)
-if (shouldScrape("headwears")) savedData.armours = await getArmours(page)
-if (shouldScrape("locations")) savedData.armours = await getArmours(page)
-if (shouldScrape("potions")) savedData.armours = await getArmours(page)
-if (shouldScrape("rings")) savedData.armours = await getArmours(page)
-if (shouldScrape("scrolls")) savedData.armours = await getArmours(page)
-if (shouldScrape("shields")) savedData.armours = await getArmours(page)
-if (shouldScrape("spells")) savedData.armours = await getArmours(page)
+if (shouldScrape("arrows")) savedData.arrows = await getArmours(page)
+// if (shouldScrape("bosses")) savedData.bosses = await getArmours(page)
+// if (shouldScrape("cloaks")) savedData.cloaks = await getArmours(page)
+// if (shouldScrape("clothes")) savedData.clothes = await getArmours(page)
+// if (shouldScrape("coatings")) savedData.coatings = await getArmours(page)
+if (shouldScrape("companions")) savedData.companions = getCompanions()
+// if (shouldScrape("elixirs")) savedData.elixirs = await getArmours(page)
+// if (shouldScrape("footwears")) savedData.footwears = await getArmours(page)
+// if (shouldScrape("grenades")) savedData.grenades = await getArmours(page)
+// if (shouldScrape("handwears")) savedData.handwears = await getArmours(page)
+// if (shouldScrape("headwears")) savedData.headwears = await getArmours(page)
+if (shouldScrape("locations")) savedData.locations = getLocations()
+// if (shouldScrape("potions")) savedData.potions = await getArmours(page)
+// if (shouldScrape("rings")) savedData.rings = await getArmours(page)
+// if (shouldScrape("scrolls")) savedData.scrolls = await getArmours(page)
+// if (shouldScrape("shields")) savedData.shields = await getArmours(page)
+// if (shouldScrape("spells")) savedData.spells = await getArmours(page)
 // TODO ends
 if (shouldScrape("weapons")) savedData.weapons = await getWeapons(page)
 
--- packages/wiki-scraper/src/pages/companions.ts
+++ packages/wiki-scraper/src/pages/companions.ts
@@ -4,55 +4,57 @@
 import type { Companion } from "@bg3-walkthrough-planner/types"
 
 // https://bg3.wiki/wiki/Companions
-export const companions: Companion[] = [
-  {
-    name: "Astarion",
-    icon: "https://bg3.wiki/w/images/thumb/1/1b/Portrait_Astarion.png/180px-Portrait_Astarion.png",
-    wikiLink: "https://bg3.wiki/wiki/Astarion",
-  },
-  {
-    name: "Gale",
-    icon: "https://bg3.wiki/w/images/thumb/1/11/Portrait_Gale.png/180px-Portrait_Gale.png",
-    wikiLink: "https://bg3.wiki/wiki/Gale",
-  },
-  {
-    name: "Karlach",
-    icon: "https://bg3.wiki/w/images/thumb/b/b2/Portrait_Karlach.png/180px-Portrait_Karlach.png",
-    wikiLink: "https://bg3.wiki/wiki/Karlach",
-  },
-  {
-    name: "Lae'zel",
-    icon: "https://bg3.wiki/w/images/thumb/8/86/Portrait_Lae%27zel.png/180px-Portrait_Lae%27zel.png",
-    wikiLink: "https://bg3.wiki/wiki/Lae%27zel",
-  },
-  {
-    name: "Shadowheart",
-    icon: "https://bg3.wiki/w/images/thumb/7/7e/Portrait_Shadowheart.png/180px-Portrait_Shadowheart.png",
-    wikiLink: "https://bg3.wiki/wiki/Shadowheart",
-  },
-  {
-    name: "Wyll",
-    icon: "https://bg3.wiki/w/images/thumb/6/6d/Portrait_Wyll.png/180px-Portrait_Wyll.png",
-    wikiLink: "https://bg3.wiki/wiki/Wyll",
-  },
-  {
-    name: "Halsin",
-    icon: "https://bg3.wiki/w/images/thumb/f/fe/Portrait_Halsin.png/180px-Portrait_Halsin.png",
-    wikiLink: "https://bg3.wiki/wiki/Halsin",
-  },
-  {
-    name: "Minthara",
-    icon: "https://bg3.wiki/w/images/thumb/8/8e/Portrait_Minthara_%28263%29.png/120px-Portrait_Minthara_%28263%29.png",
-    wikiLink: "https://bg3.wiki/wiki/Minthara",
-  },
-  {
-    name: "Minsc",
-    icon: "https://bg3.wiki/w/images/thumb/d/d6/Portrait_Minsc.png/180px-Portrait_Minsc.png",
-    wikiLink: "https://bg3.wiki/wiki/Minsc",
-  },
-  {
-    name: "Jaheira",
-    icon: "https://bg3.wiki/w/images/thumb/a/a8/Portrait_Jaheira.png/180px-Portrait_Jaheira.png",
-    wikiLink: "https://bg3.wiki/wiki/Jaheira",
-  },
-]
+export function getCompanions(): Companion[] {
+  return [
+    {
+      name: "Astarion",
+      icon: "https://bg3.wiki/w/images/thumb/1/1b/Portrait_Astarion.png/180px-Portrait_Astarion.png",
+      wikiLink: "https://bg3.wiki/wiki/Astarion",
+    },
+    {
+      name: "Gale",
+      icon: "https://bg3.wiki/w/images/thumb/1/11/Portrait_Gale.png/180px-Portrait_Gale.png",
+      wikiLink: "https://bg3.wiki/wiki/Gale",
+    },
+    {
+      name: "Karlach",
+      icon: "https://bg3.wiki/w/images/thumb/b/b2/Portrait_Karlach.png/180px-Portrait_Karlach.png",
+      wikiLink: "https://bg3.wiki/wiki/Karlach",
+    },
+    {
+      name: "Lae'zel",
+      icon: "https://bg3.wiki/w/images/thumb/8/86/Portrait_Lae%27zel.png/180px-Portrait_Lae%27zel.png",
+      wikiLink: "https://bg3.wiki/wiki/Lae%27zel",
+    },
+    {
+      name: "Shadowheart",
+      icon: "https://bg3.wiki/w/images/thumb/7/7e/Portrait_Shadowheart.png/180px-Portrait_Shadowheart.png",
+      wikiLink: "https://bg3.wiki/wiki/Shadowheart",
+    },
+    {
+      name: "Wyll",
+      icon: "https://bg3.wiki/w/images/thumb/6/6d/Portrait_Wyll.png/180px-Portrait_Wyll.png",
+      wikiLink: "https://bg3.wiki/wiki/Wyll",
+    },
+    {
+      name: "Halsin",
+      icon: "https://bg3.wiki/w/images/thumb/f/fe/Portrait_Halsin.png/180px-Portrait_Halsin.png",
+      wikiLink: "https://bg3.wiki/wiki/Halsin",
+    },
+    {
+      name: "Minthara",
+      icon: "https://bg3.wiki/w/images/thumb/8/8e/Portrait_Minthara_%28263%29.png/120px-Portrait_Minthara_%28263%29.png",
+      wikiLink: "https://bg3.wiki/wiki/Minthara",
+    },
+    {
+      name: "Minsc",
+      icon: "https://bg3.wiki/w/images/thumb/d/d6/Portrait_Minsc.png/180px-Portrait_Minsc.png",
+      wikiLink: "https://bg3.wiki/wiki/Minsc",
+    },
+    {
+      name: "Jaheira",
+      icon: "https://bg3.wiki/w/images/thumb/a/a8/Portrait_Jaheira.png/180px-Portrait_Jaheira.png",
+      wikiLink: "https://bg3.wiki/wiki/Jaheira",
+    },
+  ]
+}
