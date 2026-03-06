849cf5dee5cd074090ec9e99854bc676b62831c1
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2026-03-06 20:46:10 +0100

    Revert "Reapply "resizable sidebar""
    This reverts commit eb6ab6a4135ca8b6cad85d28e04cf6ea73ec4cce.

--- packages/planner/src/App.vue
+++ packages/planner/src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import { useEventListener } from "@vueuse/core"
 import { storeToRefs } from "pinia"
-import Splitter from "primevue/splitter"
-import SplitterPanel from "primevue/splitterpanel"
 import Toast from "primevue/toast"
-import { computed } from "vue"
 
 import FloatingDataFilters from "@/data-filters/FloatingDataFilters.vue"
 import TheFlow from "@/flowchart/TheFlow.vue"
@@ -33,37 +30,22 @@ function warnWhenLeavingTheSiteWithUnsavedChanges(event: BeforeUnloadEvent) {
 }
 
 const breakpoints = useBreakpoints()
-const isSmallScreen = breakpoints.smallerOrEqual("sm")
-const splitterLayout = computed(() => {
-  return isSmallScreen.value ? "vertical" : "horizontal"
-})
-const splitterStorageKey = import.meta.env.DEV ? null : "planner:splitter-size"
+const showFloatingFilters = breakpoints.smallerOrEqual("sm")
 
 fetchStoreData()
 </script>
 
 <template>
   <div>
-    <main class="grid text-[#d2d2d2]">
+    <main
+      class="grid h-dvh text-[#d2d2d2] max-lg:grid-rows-[55%_auto] lg:grid-cols-[auto_528px]"
+    >
       <FloatingDataFilters
-        v-if="isSmallScreen"
+        v-if="showFloatingFilters"
         class="absolute top-0 right-0 left-0 z-20"
       />
-
-      <Splitter
-        :layout="splitterLayout"
-        :state-key="splitterStorageKey"
-        state-storage="local"
-        class="h-dvh"
-      >
-        <SplitterPanel :size="75">
-          <TheFlow />
-        </SplitterPanel>
-
-        <SplitterPanel v-if="storeLoaded">
-          <TheSidebar class="h-dvh lg:min-w-[506px] 3xl:min-w-[654px]" />
-        </SplitterPanel>
-      </Splitter>
+      <TheFlow />
+      <TheSidebar v-if="storeLoaded" />
     </main>
 
     <Toast position="bottom-left" />
