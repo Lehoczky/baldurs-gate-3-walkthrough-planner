8bb9d12ee211963895e362c726c64d734a2bebba
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2026-03-06 20:03:03 +0100

    open markdown links in new tab

--- packages/planner/src/flowchart/SpecialNodes/useMarkdownIt.ts
+++ packages/planner/src/flowchart/SpecialNodes/useMarkdownIt.ts
@@ -19,8 +19,26 @@ let md: ReturnType<typeof markdownit>
  */
 export function useMarkdownIt(text: Ref<string>) {
   if (!md) {
-    md = markdownit({ linkify: true })
+    md = markdownit({ linkify: true, breaks: true })
     md.linkify.set({ fuzzyEmail: false })
+    renderLinkTargetBlank(md)
   }
   return computed(() => md.render(text.value))
+}
+
+/**
+ *
+ * See: https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+ */
+function renderLinkTargetBlank(md: markdownit) {
+  const defaultRender =
+    md.renderer.rules.link_open ||
+    function (tokens, idx, options, _env, self) {
+      return self.renderToken(tokens, idx, options)
+    }
+
+  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    tokens[idx].attrSet("target", "_blank")
+    return defaultRender(tokens, idx, options, env, self)
+  }
 }
