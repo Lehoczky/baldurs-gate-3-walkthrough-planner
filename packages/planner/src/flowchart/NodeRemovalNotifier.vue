c2ccd9a2eafd9dfe261ea096fccc1937d180bbea
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2026-05-02 22:45:10 +0200

    one notification for multiple deletion

--- packages/planner/src/flowchart/NodeRemovalNotifier.vue
+++ packages/planner/src/flowchart/NodeRemovalNotifier.vue
@@ -10,6 +10,8 @@ const toast = useToast()
 const selections: Record<string, GraphNode> = {}
 
 onNodesChange((changes) => {
+  const deletedNodes: GraphNode[] = []
+
   for (const change of changes) {
     if (change.type === "select") {
       const { id, selected } = change
@@ -20,24 +22,41 @@ onNodesChange((changes) => {
       }
     } else if (change.type === "remove") {
       const node = selections[change.id]
+      deletedNodes.push(node)
       delete selections[change.id]
-      showNodeDeletedToast(node)
     }
+  }
+
+  if (deletedNodes.length) {
+    showNodeDeletedToast(deletedNodes)
   }
 })
 
-function showNodeDeletedToast(node: GraphNode) {
+function showNodeDeletedToast(nodes: GraphNode[]) {
+  const firstNode = nodes[0]
+  if (!firstNode) {
+    // Somehow when we delete every node, we get undefined for every node
+    return
+  }
+  let summary = `Node deleted: ${firstNode.data?.name ?? firstNode.type}`
+  if (nodes.length > 1) {
+    summary = `${summary}, and ${nodes.length - 1} others`
+  }
+
   const message: ToastMessageOptions = {
     severity: "error",
     group: "node-delete",
+    summary,
     life: 8000,
     detail: {
       undo: () => {
-        addNodes(node)
+        addNodes(nodes)
         // Vue-flow adds the node back selected, but without firing
         // the selection event, so we add the node the selection array
         // manually.
-        selections[node.id] = findNode(node.id)
+        for (const node of nodes) {
+          selections[node.id] = findNode(node.id)
+        }
         toast.remove(message)
       },
     },
@@ -52,7 +71,7 @@ function showNodeDeletedToast(node: GraphNode) {
       <div class="flex flex-1 items-center gap-2">
         <div class="flex flex-1 items-center gap-1.5 text-lg font-medium">
           <span class="i-lucide-trash-2"></span>
-          <span>Node deleted</span>
+          <span>{{ message.summary }}</span>
         </div>
         <Button severity="danger" size="small" @click="message.detail.undo()">
           Undo
